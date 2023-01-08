928d67b4bbd4104dbfd7ecbb35857c817138fff9
Ryan Mitsu <anuchiha387@gmail.com>
2023-01-08 20:03:45 +0800

    update MainActivity.java

--- app/src/main/java/com/termux/x11/MainActivity.java
+++ app/src/main/java/com/termux/x11/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_activity);
-        setFullScreen();
+        setFullScreenForExternalDisplay();
 
         kbd = findViewById(R.id.additionalKbd);
 	frm = findViewById(R.id.frame);
@@ -81,25 +81,6 @@ public class MainActivity extends AppCompatActivity {
         return getIntent().getBooleanExtra(REQUEST_LAUNCH_EXTERNAL_DISPLAY, false);
     }
     
-private void setFullScreen(){
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        getWindow().setDecorFitsSystemWindows(false);
-        WindowInsetsControllercontroller=getWindow().getInsetsController();
-        if(controller != null) {
-controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());    controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
- 
-        }
-    }
-    else{
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-    }
-}
-
     private void setFullScreenForExternalDisplay() {
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
